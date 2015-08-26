f1ace1d09c732ea353d4173c7e2a23aa7c740ffb
Justin <justincyork@gmail.com>
2015-08-26 20:30:33 +0000

    add sdk

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var express = require('express'),
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
 app.get('/', function(req, res){
   res.render('index');
 });
