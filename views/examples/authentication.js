03b5317ef2aaebf6243c48e86f54d65a31b852e1
Justin <justincyork@gmail.com>
2015-08-27 17:24:12 +0000

    fix auth bugs

--- views/examples/authentication.js
+++ views/examples/authentication.js
@@ -1,11 +1,17 @@
+// This is a shortcut for $(document).ready()
 $(function(){
+  
+  // When the user clicks the "Sign In" button, call the `signIn` method.
   $('#signinBtn').click(signIn);
 });
 
+// Function is called when the user clicks the "Sign In" button.
 function signIn(){
+  
+  // Request an access token (login)
   client.getAccessToken().then(function(accessToken){
+    
+    // Display the access token
     $('#accessToken').text(accessToken);
-  }).catch(function(e){
-    console.error(e);
   });
 }
