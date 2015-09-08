3b06e8544c9d12506b0d83297069d9dfa09117d5
Justin <justincyork@gmail.com>
2015-09-08 16:52:55 +0000

    installation instructions

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# javascript-sdk-sample-app
-Sample app for the FamilySearch JavaScript SDK
+# FamilySearch JavaScript SDK Sample App
+
+Sample app for the [FamilySearch JavaScript SDK](https://github.com/FamilySearch/familysearch-javascript-sdk)
+
+## Installation
+
+1. Install [Node.js](https://nodejs.org/en/)
+2. Clone this repository
+3. Install the app's dependencies: `npm install`
+4. Run the app: `node app.js`
+5. The app will be running at `localhost:8080`
--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express'),
     app = express(),
-    path = require('path'),
-    fs = require('fs');
+    path = require('path');
     
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -17,7 +16,7 @@ app.get('/examples/:example', function(req, res){
   });
 });
 
-var server = app.listen(process.env.PORT, process.env.IP, function () {
+var server = app.listen(process.env.PORT || 8080, process.env.IP, function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log('listening at http://%s:%s', host, port);
